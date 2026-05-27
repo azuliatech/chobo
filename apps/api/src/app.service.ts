02aaea50803aa5002695704053d22094943ab579
Brian Ibekwe <brianibekwe241@gmail.com>
2026-05-27 20:34:08 +0100

    feat: make API welcome message environment-aware

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const env = process.env.NODE_ENV || 'development';
+    return `KashAm API is fully operational in [${env.toUpperCase()}] mode! 🚀`;
   }
 }
